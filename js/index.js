ac1b8ef96f30bf698c406d1ad224fdcc199b8d8b
ajb85 <ajbrush85@gmail.com>
2019-01-29 15:08:10 -0500

    Nav items with href=# no longer refresh the page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// Prevent nav items from refreshing the page
+document.querySelectorAll("nav a").forEach(anchor => {
+  anchor.addEventListener("click", e => {
+    // Target anchors that refresh the page...aka have href="#"
+    // Otherwise, let me behave normally
+    if (e.target.href === "#") e.preventDefault();
+  }); // Event listener
+}); // forEach
