4956670d518d66f282e967cec16e6523ac00c777
ajb85 <ajbrush85@gmail.com>
2019-01-29 15:35:47 -0500

    Demo'd propagation on parent/child and how to stop it

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-// Your code goes here
-
-// Prevent nav items from refreshing the page
+// MVP GOAL: Prevent nav items from refreshing the page
 document.querySelectorAll("nav a").forEach(anchor => {
   anchor.addEventListener("click", e => {
     // Target anchors that refresh the page...aka have href="#"
@@ -8,3 +6,17 @@ document.querySelectorAll("nav a").forEach(anchor => {
     if (e.target.href === "#") e.preventDefault();
   }); // Event listener
 }); // forEach
+// end MVP
+
+// MVP GOAL: use stopProp to prevent event from bubbling
+// Parent event listener
+document.querySelector("header div").addEventListener("click", e => {
+  console.log("Parent: ", e.target);
+});
+// Child event listener
+document.getElementsByTagName("nav")[0].addEventListener("click", e => {
+  // mouseenter instead of mouseover so children (anchor tags) won't inherent
+  console.log("Child: ", e.target);
+  e.stopPropagation(); // child only!  Commented out: logs Child & Parent.
+});
+// end MVP
