c717536ca1f2d3dd94dc48242c1f663b288d22d9
ajb85 <ajbrush85@gmail.com>
2019-01-29 16:15:11 -0500

    Created a loop that randomly assigns every page element a listener

--- js/index.js
+++ js/index.js
@@ -20,3 +20,56 @@ document.getElementsByTagName("nav")[0].addEventListener("click", e => {
   e.stopPropagation(); // child only!  Commented out: logs Child & Parent.
 });
 // end MVP
+
+// MVP GOAL: 10 unique event listeners
+
+// This code is just to have some fun!  It adds a random listener to every element on the page and logs when its triggered!
+
+// const events = {
+//   focus: true,
+//   blur: true,
+//   cut: true,
+//   copy: true,
+//   paste: true,
+//   auxclick: true,
+//   click: true,
+//   contextmenu: true,
+//   dblclick: true,
+//   mousedown: true,
+//   mouseenter: true,
+//   mouseleave: true,
+//   mousemove: true,
+//   mouseover: true,
+//   mouseout: true,
+//   mouseup: true,
+//   //select: { form: ["input", "textarea"] }, // Requires a form, which I don't have
+//   wheel: true,
+//   drag: true,
+//   dragend: true,
+//   dragstart: true
+// };
+// const allElements = document.body.getElementsByTagName("*");
+// const elementCount = allElements.length;
+// let newEvents = Object.keys(events);
+// const eventLog = {};
+//
+// Array.from(allElements).forEach(element => {
+//   const i = Math.round(Math.random() * (newEvents.length - 1));
+//   if (!newEvents.length) {
+//     newEvents = Object.keys(events);
+//   }
+//   element.addEventListener(
+//     newEvents[i],
+//     e =>
+//       console.log(
+//         `${e.target.tagName} ${e.target.className} has been triggered by ${
+//           [...newEvents][i]
+//         }`
+//       ) // Console log
+//   ); // listener
+//   eventLog[`${element.tagName} ${element.className}`] = [...newEvents][i];
+//   newEvents.splice(i, 1); // Remove from the index
+// });
+//
+// function logEvent(e, event) {}
+// console.log("Events: ", eventLog);
