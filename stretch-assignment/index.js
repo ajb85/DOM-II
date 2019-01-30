aee8a556d5a9419426ea0d5768d5fe4151f50f6d
ajb85 <ajbrush85@gmail.com>
2019-01-29 20:30:02 -0500

    EventListener code better organized.  Boxes resort when clicked

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -18,21 +18,36 @@ function removeTimerEvents() {
 Array.from(document.getElementsByClassName("block")).forEach(block => {
   block.addEventListener(
     "mousedown",
+    // Closure to protect the x position
     (() => {
-      let xPos = 0;
-      return function(e) {
-        const interval = 50;
-        let movement = setInterval(moveBoxes, interval);
-        window.addEventListener("mouseup", () => clearInterval(movement));
-
-        function moveBoxes() {
-          //console.log(e.target);
-          xPos += 1;
-          e.target.style.transform = `matrix(0, -1, 1, 0, ${xPos}, 0)`;
-        } // moveBoxes
-      }; // Nested Function
+      // Objects are references so .bind will pass the REFERENCE instead
+      // of the static value if I were to use x = 0
+      let pos = { x: 0 };
+      return manageInterval.bind(this, pos); // Nested Function
     })()
   );
 }); // foreach
 
-function manageInterval(e) {}
+function manageInterval(pos, e) {
+  const interval = 50;
+  let movement = setInterval(moveBoxes, interval);
+  window.addEventListener("mouseup", () => {
+    clearInterval(movement);
+    const order = {
+      "block--red": 1,
+      "block--blue": 2,
+      "block--green": 3,
+      "block--pink": 4,
+      "block--gray": 5
+    };
+    const color = e.target.className.split(" ")[1];
+    e.target.style.order = order[color];
+  });
+
+  function moveBoxes() {
+    //console.log(e.target);
+    pos.x += 2;
+    e.target.style.order = 0;
+    e.target.style.transform = `matrix(0, -1, 1, 0, ${pos.x}, 0)`;
+  } // moveBoxes
+}
