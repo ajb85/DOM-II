fc9ae06f8de4e10220d8c9a955b46299f5b89ec2
ajb85 <ajbrush85@gmail.com>
2019-01-30 10:54:16 -0500

    Boxes can now be clicked mulitple times while falling.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -22,32 +22,57 @@ Array.from(document.getElementsByClassName("block")).forEach(block => {
     (() => {
       // Objects are references so .bind will pass the REFERENCE instead
       // of the static value if I were to use x = 0
-      let pos = { x: 0 };
-      return manageInterval.bind(this, pos); // Nested Function
-    })()
-  );
+      let pos = { x: 0, y: 0, id: { backTimer: 0, fallTimer: 0 } };
+      return manageInterval.bind(this, pos, block);
+    })() // self-invoking
+  ); // eventListener
 }); // foreach
 
-function manageInterval(pos, e) {
-  const interval = 50;
-  let movement = setInterval(moveBoxes, interval);
-  window.addEventListener("mouseup", () => {
-    clearInterval(movement);
-    const order = {
-      "block--red": 1,
-      "block--blue": 2,
-      "block--green": 3,
-      "block--pink": 4,
-      "block--gray": 5
-    };
-    const color = e.target.className.split(" ")[1];
-    e.target.style.order = order[color];
-  });
+function manageInterval(pos, block, e) {
+  // Verify no intervals are running.  If found, kill it
+  if (pos.id.fallTimer) {
+    clearTimer(pos, "fallTimer");
+    //window.removeEventListener("mouseup", onBoxDeclick);
+  }
 
+  const order = {
+    "block--red": 0,
+    "block--blue": 1,
+    "block--green": 2,
+    "block--pink": 3,
+    "block--gray": 4
+  };
+  const color = e.target.className.split(" ")[1];
+  const interval = 50;
+  const maxY = -order[color] * 100 - 16 * order[color];
+  let movement = setInterval(moveBoxes, interval);
+
+  window.addEventListener("mouseup", onBoxDeclick, { once: true });
+
+  function onBoxDeclick() {
+    clearInterval(movement);
+    //block.removeEventListener("mousedown", onBoxClick);
+    pos.id.fallTimer = setInterval(boxFalls.bind(this, onBoxDeclick), interval);
+  }
   function moveBoxes() {
-    //console.log(e.target);
+    // Manage box position
     pos.x += 2;
-    e.target.style.order = 0;
-    e.target.style.transform = `matrix(0, -1, 1, 0, ${pos.x}, 0)`;
+    pos.y = pos.y > maxY ? (pos.y -= 3) : maxY;
+    e.target.style.transform = `matrix(0, -1, 1, 0, ${pos.x}, ${pos.y})`;
   } // moveBoxes
+
+  function boxFalls(onBoxDeselect) {
+    pos.y = pos.y < 0 ? (pos.y += 8) : 0;
+    e.target.style.transform = `matrix(0, -1, 1, 0, ${pos.x}, ${pos.y})`;
+    if (!pos.y) {
+      clearTimer(pos, "fallTimer");
+      //window.removeEventListener("mouseup", onBoxDeclic);
+    }
+  }
+}
+
+function clearTimer(pos, timer) {
+  console.log(`Clearing ${timer}: ${pos.id[timer]}`);
+  clearInterval(pos.id[timer]);
+  pos.id[timer] = 0;
 }
